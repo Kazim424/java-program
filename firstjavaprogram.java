581955be35151c2e19d3acefd7135777e908a9c0
Kazim424 <120630543+Kazim424@users.noreply.github.com>
2023-01-22 06:36:28 -0500

    This is first java hello world program
    This is first java hello world program.

---
+++ firstjavaprogram.java
@@ -0,0 +1,10 @@
+// 1. WAP a program Hello world.
+public class firstjavaprogram{
+    // psvm
+    public static void main(String[] args){
+        // syso
+        // sout
+        System.out.println("Hello world java");
+       
+    }
+}
